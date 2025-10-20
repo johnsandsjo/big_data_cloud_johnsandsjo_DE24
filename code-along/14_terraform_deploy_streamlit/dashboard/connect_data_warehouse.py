a7297c9bd0710e8db3dd2089f7d4d21aa924bb95
John Sandsjö <john.sandsjo@gmail.com>
2025-10-20 19:48:00 +0200

    deployed web dashboard

--- code-along/14_terraform_deploy_streamlit/dashboard/connect_data_warehouse.py
+++ code-along/14_terraform_deploy_streamlit/dashboard/connect_data_warehouse.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 import duckdb
-import os
 
 # data warehouse directory
-FILES_SHARE_PATH = Path("/mnt/data/job_ads.duckdb")
+DB_PATH = Path("/app/data_warehouse/job_ads.duckdb")
  
 def query_job_listings(query='SELECT * FROM marts.mart_technical_jobs'):
-    with duckdb.connect(FILES_SHARE_PATH, read_only=True) as conn:
+    with duckdb.connect(DB_PATH, read_only=True) as conn:
         return conn.query(f"{query}").df()
 
 
